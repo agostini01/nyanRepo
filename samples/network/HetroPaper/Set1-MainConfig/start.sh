62572f9c0ebd05a49df82cf12230c84e3912b598
Yifan Sun <sunyifan112358@gmail.com>
2015-08-20 16:06:52 -0400

    a bash file that runs multiple benchmarks

---
+++ samples/network/HetroPaper/Set1-MainConfig/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SUBDIRS="
+MatrixMultiplication
+BinarySearch
+"
+
+for dir in $SUBDIRS
+do
+	cd $dir
+	nohup ../../../../../bin/m2s --ctx-config ctx-config \
+		--si-sim detailed --si-config ../si-config \
+		--mem-config ../mem-si --net-config ../net-ideal-si \
+		--net-report net.report \
+		>nohup.out 2>&1 
+	cd ..
+done
