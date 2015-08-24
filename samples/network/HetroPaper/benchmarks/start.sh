19e53bea4f740388c6393caacf0315cabf3e9215
Yifan Sun <sunyifan112358@gmail.com>
2015-08-23 22:27:40 -0400

    make benchmarks to run in a separate directory

--- samples/network/HetroPaper/benchmarks/start.sh
+++ samples/network/HetroPaper/benchmarks/start.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
 SUBDIRS="
-MatrixMultiplication
+AESEncryptDecrypt
 BinarySearch
 BinomialOption
 BitonicSort
 BlackScholes
+BoxFilter
+DCT
+DwtHaar1D
+EigenValue
+FastWalshTransform
 FFT
 FloydWarshall
+Histogram
+MatrixMultiplication
+MatrixTranspose
+MersenneTwister
+PrefixSum
+QuasiRandomSequence
+RadixSort
+RecursiveGaussian
+Reduction
+ScanLargeArrays
+SimpleConvolution
+SobelFilter
 "
 
 for dir in $SUBDIRS
 do
 	cd $dir
 	nohup ../../../../../bin/m2s --ctx-config ctx-config \
-		--si-sim detailed --si-config $1si-config \
-		--mem-config $1mem-si --net-config $1net-ideal-si \
+		--si-sim detailed --si-config ../$1si-config \
+		--mem-config ../$1mem-si --net-config ../$1net-ideal-si \
 		--net-report net.report \
 		--mem-report mem.report \
 		>nohup.out 2>&1 &
